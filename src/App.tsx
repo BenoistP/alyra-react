53f2bf64dca081859f0cdd994e6ba89a81243fda
BenoistP <75934369+BenoistP@users.noreply.github.com>
2024-06-20 12:52:18 +0200

    small updates

--- src/App.tsx
+++ src/App.tsx
@@ -4,28 +4,30 @@ import { Dashboard } from './pages/Dashboard';
 import { useEffect, useState } from 'react';
 import { useWallet } from '@solana/wallet-adapter-react';
 import { getSolanaBalance } from './helpers/solana.helper';
-
-
+import Balance from './pages/Balance';
 
 function App() {
 
   const wallet = useWallet();
-  const [solanaBalance, setsolanaBalance] = useState<number>(0)
+  const [solanaBalance, setSolanaBalance] = useState<number | null>(null);
+
   useEffect(() => {
     if (wallet.publicKey) {
-      getSolanaBalance(wallet.publicKey.toBase58()).then((balance) => setsolanaBalance(balance))
+      getSolanaBalance(wallet.publicKey.toBase58())
+        .then((balance) => setSolanaBalance(balance));
+    } else {
+      setSolanaBalance(null);
     }
-  
     return () => {
       //
     }
-  }, [wallet.publicKey])
-  
+  }, [wallet.publicKey]);
 
   return (
     <div className="App">
       <div className="header">
         <div className='wallet'>
+          <Balance balance={solanaBalance}/>
           <WalletMultiButton></WalletMultiButton>
         </div>
       </div>
